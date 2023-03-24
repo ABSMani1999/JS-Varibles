b05193da0a57875569b9d42496a3ac11032a08ac
Balasubramanian1999 <absmani1999@gmail.com>
2023-03-24 20:34:56 +0530

    new file add

---
+++ main.js
@@ -0,0 +1,43 @@
+//variable type
+//var
+var x=5;
+var y=10;
+var x=22;
+
+document.write(x+y);
+document.write("<br>");
+
+//let
+
+let a=8;
+let b=12;
+
+document.write(a+b);
+document.write("<br>");
+
+//const
+
+const price1=5;
+const price=13;
+
+document.write (price1+price);
+document.write("<br>");
+
+//var+let-const
+
+var p=12;
+let q=8;
+const r=9;
+
+document.write(p+q+r);
+document.write("<br>");
+document.write(typeof p+q+r);
+document.write("<br>");
+
+//nothing
+
+x=5;
+y=12;
+
+document.write(x-y);
+document.write("<br>");
